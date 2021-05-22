617dce5026a555bc84adf0bfef8c2ef7840b343e
Özcan Çaparoğlu <ozcan.caparoglu@hotmail.com>
2021-05-22 23:16:49 +0300

    healtcheck düzeltildi

---
+++ CategoryService.Api/Controllers/HealthCheckController.cs
@@ -0,0 +1,27 @@
+﻿using CategoryService.ApiContract;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace CategoryService.Api.Controllers
+{
+    [Route("healthcheck")]
+    [ApiController]
+    public class HealthCheckController : ControllerBase
+    {
+        private readonly IMediator mediator;
+
+        public HealthCheckController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        [HttpPost]
+        [ProducesResponseType(200, Type = typeof(Result<object>))]
+        public async Task<IActionResult> Index(HealthCheckQuery request)
+        {
+            var result = await mediator.Send(request);
+            return Ok(result);
+        }
+    }
+}
